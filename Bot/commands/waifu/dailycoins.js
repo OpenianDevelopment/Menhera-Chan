5930a47c3731b25d5851c3c3db744ea3dc596138
NORO3618 <66942330+NORO3618@users.noreply.github.com>
2021-01-19 18:55:21 +0200

    Update dailycoins.js

--- Bot/commands/waifu/dailycoins.js
+++ Bot/commands/waifu/dailycoins.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');;
 const globalFunc = require('../../function/dbfunctions');
 const DBL = require("dblapi.js");
+const {DBL_TOKEN} = require("../../botconfig.json")
 
 module.exports = {
     name: 'dailycoins',
@@ -8,7 +9,7 @@ module.exports = {
     category:'general',
     args: false,
     run:async(client,message,args)=>{
-        const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjczMTE0Mzk1NDAzMjIzMDQ1MyIsImJvdCI6dHJ1ZSwiaWF0IjoxNjA3NDM3NzIzfQ.Je4W0naACdZPsP_yWpHX06yYhKmPxbI9AFYO0MFif5o', client);
+        const dbl = new DBL(DBL_TOKEN, client);
         dbl.hasVoted(message.author.id).then(votes => {if(votes == false){message.channel.send("vote to get daily coins \nGo to https://top.gg/bot/731143954032230453 to vote!")}else{message.channel.send("You already got you daily coins! \nCome back at a later time.")}
         });
     }
