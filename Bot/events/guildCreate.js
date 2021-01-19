651ac943d2f9aff64e4a93a9f8dc041679b6ae67
NORO3618 <66942330+NORO3618@users.noreply.github.com>
2021-01-19 19:06:28 +0200

    Update guildCreate.js

--- Bot/events/guildCreate.js
+++ Bot/events/guildCreate.js
@@ -1,7 +1,7 @@
 const {addGuildSetting, initWarn, initXP, initModeration, initXPBlacklist} = require('../function/dbfunctions')
 const { initWelcomeRole, initNews, initWelcome, initAntispam } = require('../function/dbfunctions(2)')
 const discordwh = require("discord-webhook-messages");
-const webhook = new discordwh.Webhook("https://discordapp.com/api/webhooks/752795872592527381/I3eBiUR16GHZ5Xx6DFU0bKSnILbwqDvXgGwPCnGWFZxVrb3MrlOYyGsMiTDtSggkzCR1");
+const webhook = new discordwh.Webhook(/*the discord webhook url*/);
 
 module.exports = (client,guild) => {
     addGuildSetting(guild.id);
