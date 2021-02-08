099c5bbe190305439de938286caa7c94a2b7f6e3
NORO3618 <66942330+NORO3618@users.noreply.github.com>
2021-02-08 18:37:43 +0200

    Update guildDelete.js

--- Bot/events/guildDelete.js
+++ Bot/events/guildDelete.js
@@ -1,8 +1,10 @@
 const {removeGuildSetting, removeXPBlacklist, removeGuildModerations, removeGuildWarns, removeGuildXP} = require('../function/dbfunctions')
 const { deleteWelcomeRole, deleteNews, deleteWelcome, deleteAntispam } = require('../function/dbfunctions(2)')
 const discordwh = require("discord-webhook-messages");
-
-const webhook = new discordwh.Webhook(/*the discord webhook url*/);
+const webhookURL = ""; /*the discord webhook url*/
+if(webhookURL) {
+    const webhook = new discordwh.Webhook(webhookURL);
+}
 
 module.exports = (client,guilds) => {
     removeGuildSetting(guilds.id);
@@ -13,7 +15,6 @@ module.exports = (client,guilds) => {
     deleteWelcomeRole(guilds.id)
     deleteNews(guilds.id);
     deleteWelcome(guilds.id);
-    deleteAntispam(guilds.id)
-
-    webhook.sendMessage(`**[REMOVE]** ${guilds.name}`);
+    deleteAntispam(guilds.id);
+    webhookURL ? webhook.sendMessage(`**[REMOVE]** ${guilds.name}`) : null;
 }
