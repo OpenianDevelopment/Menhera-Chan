c28c05c9a0ce4973ce04f289ae1f48420ab01422
july12123 <52258148+july12123@users.noreply.github.com>
2021-03-02 20:14:59 -0500

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -20,3 +20,11 @@ client.cooldowns = new Map();
 
 client.login(token) //login
 
+/*
+Copyright (C) 2021  Major Senpai スレーブマスター#1091, Julio_#7057, and Noro#4477
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+*/
