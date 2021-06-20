ebe358ed806848996a1ba56cd039fa869503d2be
rohank05 <rohan.shuvam@gmail.com>
2021-06-20 14:52:20 +0530

    updated rank command

--- src/commands/leveling/RankCommand.ts
+++ src/commands/leveling/RankCommand.ts
@@ -1,7 +1,9 @@
-import { Message, MessageEmbed } from "discord.js";
+import { Message } from "discord.js";
 import DiscordClient from "../../client/client";
 import { getLevel } from "../../database/functions/levelingOperation";
-import { getRankCard } from "../../utils/functions/levelingFunctions";
+import { RankCard } from "../../images/rankCard";
+
+import { userXP } from "../../utils/interfaces/levelingInterfaces";
 import { BaseCommand } from "../../utils/structures";
 
 export default class RankCommand extends BaseCommand {
@@ -18,13 +20,24 @@ export default class RankCommand extends BaseCommand {
 	async run(client: DiscordClient, message: Message, args: Array<String>) {
 		const member = message.mentions.users.first() || message.author;
 		const GuildUsersXP = await getLevel(message.guild?.id);
-		const userIndex = GuildUsersXP.findIndex((e) => {
-			e.user === member.id;
-		});
-		const rankCard = await getRankCard(
-			member,
-			GuildUsersXP[userIndex],
-			userIndex + 1
+		const usersXP = GuildUsersXP.users.sort(
+			(a: userXP, b: userXP) => b.xp - a.xp
 		);
+		const userIndex = usersXP.findIndex((e) => {
+			return e.user === member.id;
+		});
+
+		const rankcardData = new RankCard()
+			.setUsername(message.author.username)
+			.setDiscrim(message.author.discriminator)
+			.setLevel(usersXP[userIndex].level)
+			.setMinXP(Math.floor(Math.pow(usersXP[userIndex].level, 2) / 0.01))
+			.setMaxXP(Math.floor(Math.pow(usersXP[userIndex].level + 1, 2) / 0.01))
+			.setXP(usersXP[userIndex].xp)
+			.setAvatar(message.author.displayAvatarURL({ format: "png" }));
+		const rankcard = await rankcardData.build();
+		message.reply({
+			files: [rankcard],
+		});
 	}
 }
