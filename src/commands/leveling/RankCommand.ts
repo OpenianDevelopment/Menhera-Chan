3ca8b42208ef9a2687d59a9fba75c2c7e289494f
rohank05 <rohan.shuvam@gmail.com>
2021-06-25 20:05:00 +0530

    Added fonts and changed rank card look
    Added more customization to rankcard

--- src/commands/leveling/RankCommand.ts
+++ src/commands/leveling/RankCommand.ts
@@ -1,43 +1,58 @@
-import { Message } from "discord.js";
+import {Message, MessageEmbed} from "discord.js";
 import DiscordClient from "../../client/client";
-import { getLevel } from "../../database/functions/levelingOperation";
-import { RankCard } from "../../images/rankCard";
+import {getLevel} from "../../database/functions/levelingOperation";
+import {RankCard} from "../../images/rankCard";
 
-import { userXP } from "../../utils/interfaces/levelingInterfaces";
-import { BaseCommand } from "../../utils/structures";
+import {userXP} from "../../utils/interfaces/levelingInterfaces";
+import {BaseCommand} from "../../utils/structures";
 
 export default class RankCommand extends BaseCommand {
-	constructor() {
-		super(
-			"rank",
-			"Get user Rank Card",
-			"leveling",
-			["r", "level"],
-			"rank [user]",
-			"rank"
-		);
-	}
-	async run(client: DiscordClient, message: Message, args: Array<String>) {
-		const member = message.mentions.users.first() || message.author;
-		const GuildUsersXP = await getLevel(message.guild?.id);
-		const usersXP = GuildUsersXP.users.sort(
-			(a: userXP, b: userXP) => b.xp - a.xp
-		);
-		const userIndex = usersXP.findIndex((e) => {
-			return e.user === member.id;
-		});
+    constructor() {
+        super(
+            "rank",
+            "Get user Rank Card",
+            "leveling",
+            ["r", "level"],
+            "rank [user]",
+            "rank"
+        );
+    }
 
-		const rankcardData = new RankCard()
-			.setUsername(message.author.username)
-			.setDiscrim(message.author.discriminator)
-			.setLevel(usersXP[userIndex].level)
-			.setMinXP(Math.floor(Math.pow(usersXP[userIndex].level, 2) / 0.01))
-			.setMaxXP(Math.floor(Math.pow(usersXP[userIndex].level + 1, 2) / 0.01))
-			.setXP(usersXP[userIndex].xp)
-			.setAvatar(message.author.displayAvatarURL({ format: "png" }));
-		const rankcard = await rankcardData.build();
-		message.reply({
-			files: [rankcard],
-		});
-	}
+    async run(client: DiscordClient, message: Message, args: Array<String>) {
+        const member = message.mentions.users.first() || message.author;
+        const GuildUsersXP = await getLevel(message.guild?.id);
+        const usersXP = GuildUsersXP.users.sort(
+            (a: userXP, b: userXP) => b.xp - a.xp
+        );
+        const userIndex = usersXP.findIndex((e) => {
+            return e.user === member.id;
+        });
+        if (userIndex < 0) {
+            const embed = new MessageEmbed().setColor('RED').setDescription("❌ I don't have any data on you at this moment. Kindly gain some XP first")
+            await message.reply({
+                embeds: [embed]
+            })
+            return;
+        }
+
+
+        const rank_card_Data = new RankCard()
+            .setUsername(message.author.username)
+            .setDiscriminator(message.author.discriminator)
+            .setLevel(usersXP[userIndex].level)
+            .setMinXP(Math.floor(Math.pow(usersXP[userIndex].level, 2) / 0.01))
+            .setMaxXP(Math.floor(Math.pow(usersXP[userIndex].level + 1, 2) / 0.01))
+            .setXP(usersXP[userIndex].xp)
+            .setAvatar(message.author.displayAvatarURL({format: "png"}))
+            .setBackground(usersXP[userIndex].background)
+            .setOpacity(usersXP[userIndex].opacity)
+            .setTrackColor(usersXP[userIndex].trackColor)
+            .setTextColor(usersXP[userIndex].textColor)
+            .setRank(userIndex + 1);
+       
+        const rank_card = await rank_card_Data.build();
+        await message.reply({
+            files: [rank_card],
+        });
+    }
 }
--- src/images/rankCard.ts
+++ src/images/rankCard.ts
@@ -1,146 +1,315 @@
-import { rankcardData } from "../utils/interfaces/levelingInterfaces";
-import { createCanvas, loadImage } from "canvas";
-import { MessageAttachment } from "discord.js";
+import {rankcardData} from "../utils/interfaces/levelingInterfaces";
+import {createCanvas, loadImage, NodeCanvasRenderingContext2D, registerFont} from "canvas";
+import {MessageAttachment} from "discord.js";
+import {readdirSync} from "fs";
+
 export class RankCard {
-	private data: rankcardData;
-	constructor() {
-		this.data = {
-			width: 1026,
-			height: 285,
-			backgroundImage:
-				"https://cdn.discordapp.com/attachments/851062487977951256/854374390694281216/rankcard.png",
+    private data: rankcardData;
 
-			trackColor: "#21cc87",
-			textColor: "#554b58",
-			username: "",
-			discrim: "",
-			avatarURL: "",
-			font: "bold 25px Arial",
-			level: 0,
-			rank: 0,
-			xp: 0,
-			MinXP: 0,
-			MaxXP: 100,
-		};
-	}
-	setBackground(imageUrl: string) {
-		this.data.backgroundImage = imageUrl;
-		return this;
-	}
-	setUsername(username: string) {
-		this.data.username = username;
-		return this;
-	}
-	setDiscrim(discrim: string) {
-		this.data.discrim = discrim;
-		return this;
-	}
-	setTrackColor(color: string) {
-		this.data.trackColor = color;
-		return this;
-	}
-	setTextColor(color: string) {
-		this.data.textColor = color;
-		return this;
-	}
-	setLevel(level: number) {
-		this.data.level = level;
-		return this;
-	}
-	setRank(rank: number) {
-		this.data.rank = rank;
-		return this;
-	}
-	setXP(xp: number) {
-		this.data.xp = xp;
-		return this;
-	}
-	setMinXP(xp: number) {
-		this.data.MinXP = xp;
-		return this;
-	}
-	setMaxXP(xp: number) {
-		this.data.MaxXP = xp;
-		return this;
-	}
-	setAvatar(url: string) {
-		this.data.avatarURL = url;
-		return this;
-	}
+    constructor() {
+        this.data = {
+            width: 1026,
+            height: 285,
+            backgroundImage:
+                "https://cdn.discordapp.com/attachments/791301593391562752/856879146175954954/rankcard2.png",
+            color: {
+                main: '#554b58',
+                track: '#21cc87'
+            },
+            opacity: 0,
+            username: "",
+            discriminator: "",
+            avatarURL: "",
+            font: {
+                bold: "Dela_Gothic_One",
+                normal: "GothicA1"
+            },
+            level: 0,
+            rank: 0,
+            xp: 0,
+            MinXP: 0,
+            MaxXP: 100,
+        };
+        const familyName: string[] = ["Dela_Gothic_One", "GothicA1"];
+        for (const family of familyName)
+            readdirSync(__dirname + `/fonts/${family}`)
+                .filter((file) => file.endsWith(".ttf"))
+                .forEach((file) => {
+                    registerFont(__dirname + `/fonts/${family}/${file}`, {
+                        family: `${family}`,
+                    });
+                });
+    }
 
-	async build() {
-		const canvas = createCanvas(this.data.width, this.data.height);
-		const ctx = canvas.getContext("2d");
-		const background = await loadImage(this.data.backgroundImage);
-		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-		ctx.beginPath();
-		ctx.lineWidth = 4;
-		ctx.strokeStyle = "#000000";
-		ctx.globalAlpha = 1;
-		ctx.fillStyle = "#000000";
-		ctx.fillRect(0, 270, 1026, 20);
-		ctx.fill();
-		ctx.globalAlpha = 1;
-		ctx.stroke();
 
-		ctx.fillStyle = this.data.trackColor;
-		ctx.globalAlpha = 1;
-		ctx.fillRect(
-			0,
-			270,
-			((this.data.xp - this.data.MinXP) / (this.data.MaxXP - this.data.MinXP)) *
-				1026,
-			20
-		);
-		ctx.fill();
-		ctx.globalAlpha = 1;
+    setBackground(imageUrl: string) {
+        this.data.backgroundImage = imageUrl;
+        return this;
+    }
 
-		ctx.font = this.data.font;
-		ctx.textAlign = "center";
-		ctx.fillStyle = this.data.textColor;
-		ctx.fillText(`XP: ${ssn(this.data.xp)}/${ssn(this.data.MaxXP)}`, 513, 260);
+    setUsername(username: string) {
+        this.data.username = username;
+        return this;
+    }
 
-		ctx.textAlign = "center";
-		ctx.fillText(`Level: ${this.data.level}`, 513, 220);
+    setDiscriminator(discriminator: string) {
+        this.data.discriminator = discriminator;
+        return this;
+    }
 
-		ctx.font = "bold underline 35px Arial";
-		ctx.textAlign = "left";
-		ctx.fillText(
-			`${shorten(this.data.username, 10)}#${this.data.discrim}`,
-			330,
-			145
-		);
+    setTrackColor(color: string) {
+        this.data.color.track = color;
+        return this;
+    }
 
-		ctx.arc(170, 135, 125, 0, Math.PI * 2, true);
-		ctx.lineWidth = 6;
-		ctx.strokeStyle = this.data.textColor;
-		ctx.stroke();
-		ctx.closePath();
-		ctx.clip();
+    setTextColor(color: string) {
+        this.data.color.main = color;
+        return this;
+    }
 
-		const avatar = await loadImage(this.data.avatarURL);
-		ctx.drawImage(avatar, 45, 10, 250, 250);
+    setLevel(level: number) {
+        this.data.level = level;
+        return this;
+    }
 
-		const attachment = new MessageAttachment(canvas.toBuffer(), "rankcard.png");
-		return attachment;
-	}
+    setRank(rank: number) {
+        this.data.rank = rank;
+        return this;
+    }
+
+    setXP(xp: number) {
+        this.data.xp = xp;
+        return this;
+    }
+
+    setMinXP(xp: number) {
+        this.data.MinXP = xp;
+        return this;
+    }
+
+    setMaxXP(xp: number) {
+        this.data.MaxXP = xp;
+        return this;
+    }
+
+    setAvatar(url: string) {
+        this.data.avatarURL = url;
+        return this;
+    }
+
+    setOpacity(opacity: number) {
+        this.data.opacity = opacity;
+        return this;
+    }
+
+    async build() {
+        const font = this.data.font;
+        const color = this.data.color;
+        const opacity = this.data.opacity;
+        const MaxXp = toAbbrev(this.data.MaxXP),
+            Xp = toAbbrev(this.data.xp),
+            rank = toAbbrev(this.data.rank);
+
+        let bg = await loadImage(this.data.backgroundImage);
+        let avatar = await loadImage(this.data.avatarURL);
+
+        // create canvas instance
+        const canvas = createCanvas(this.data.width, this.data.height);
+        const ctx = canvas.getContext("2d");
+
+        // create background
+        ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
+
+        // add 'whole' overlay
+        ctx.fillStyle = "#202225";
+        ctx.globalAlpha = opacity;
+        roundRect(ctx, 0, 0, canvas.width, canvas.height, 0, true);
+
+        //number bars
+        ctx.fillStyle = "#18191c";
+
+        roundRect(
+            ctx,
+            canvas.width - 330 + 62.5,
+            75.5 /*(- 41)*/,
+            205,
+            32.5 /* (* 2) */,
+            7
+        ); //* level in box
+        roundRect(
+            ctx,
+            canvas.width - 330 + 62.5,
+            195.5 /*(- 41)*/,
+            205,
+            32.5 /* (* 2) */,
+            7
+        ); //* rank in box
+
+        // reset transparency
+        ctx.globalAlpha = 1;
+
+        // draw username
+        ctx.font = `bold 30px ${font.bold}`;
+        ctx.fillStyle = color.main;
+        ctx.textAlign = "start";
+        const name = shorten(this.data.username, 15);
+
+        // apply username
+        ctx.fillText(`${name}`, 245.5, 150);
+
+        // draw discriminator
+        const discriminator = `${this.data.discriminator}`;
+        const nameWidth = ctx.measureText(name).width;
+        ctx.font = `32px ${font.normal}`;
+        ctx.fillStyle = color.main;
+        ctx.textAlign = "center";
+        ctx.fillText(
+            `#${discriminator}`,
+            nameWidth + 338,
+            150
+        );
+
+        //set X Axis
+        const XAxis = canvas.width - 165;
+
+        // fill level
+        ctx.font = `25px ${font.normal}`;
+        ctx.fillStyle = "#7f7f7f";
+
+        ctx.textAlign = "center";
+        ctx.fillText("Level:", XAxis, 58);
+
+
+        //fill level-num
+        ctx.font = `bold 25px ${font.bold}`;
+        ctx.fillStyle = color.main;
+        ctx.textAlign = "center";
+        ctx.fillText(this.data.level.toString(), XAxis, 101.5);
+
+        // fill rank
+        ctx.font = `25px ${font.normal}`;
+        ctx.fillStyle = "#7f7f7f";
+
+        ctx.textAlign = "center";
+        ctx.fillText("Rank:", XAxis, 180);
+
+
+        // fill rank-num
+        ctx.font = `bold 25px ${font.bold}`;
+        ctx.fillStyle = color.main;
+        ctx.textAlign = "center";
+        ctx.fillText("#" + rank.toString(), XAxis, 220.5);
+
+
+        // show Min/Max Xp
+        ctx.font = `bold 25px ${font.bold}`;
+        ctx.fillStyle = color.main;
+        ctx.textAlign = "start";
+        ctx.fillText(
+            Xp + " / " + MaxXp,
+            380 + ctx.measureText(Xp.toString()).width + 25,
+            251
+        );
+
+        // draw progress-track
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 270, 1026, 20);
+
+        // progress-bar
+        ctx.fillStyle = color.track;
+        ctx.fillRect(
+            0,
+            270,
+            ((this.data.xp - this.data.MinXP) / (this.data.MaxXP - this.data.MinXP)) *
+            1026,
+            20
+        );
+        ctx.arc(
+            ((this.data.xp - this.data.MinXP) / (this.data.MaxXP - this.data.MinXP)) *
+            1026,
+            20,
+            8,
+            0.5 * Math.PI,
+            1.5 * Math.PI,
+            true
+        );
+
+        ctx.save();
+
+        // Avatar circle
+        ctx.beginPath();
+        ctx.arc(135, 145, 100, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.clip();
+
+        // draw Avatar
+        ctx.drawImage(avatar, 35, 55, 150, 150);
+        ctx.restore();
+
+        //return the card
+        return new MessageAttachment(canvas.toBuffer(), `${this.data.username}.png`);
+
+    }
 }
+
 function shorten(text: string, len: number) {
-	if (typeof text !== "string") return "";
-	if (text.length <= len) return text;
-	return text.substr(0, len).trim() + "...";
+    if (text.length <= len) return text;
+    return text.substr(0, len).trim() + "...";
 }
 
-function ssn(n: number) {
-	if (n <= 1e4) {
-		return n;
-	}
-	if (n <= 1e6) {
-		return (n / 1e3).toFixed(1) + "k";
-	}
-	if (n <= 1e9) {
-		return (n / 1e6).toFixed(2) + "m";
-	}
+function roundRect(
+    ctx: NodeCanvasRenderingContext2D,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    radius: number,
+    fill: boolean = true,
+    stroke: boolean = false
+) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.closePath();
+    if (fill) {
+        ctx.fill();
+    }
+    if (stroke) {
+        ctx.stroke();
+    }
+    return;
+}
 
-	return n;
+function toAbbrev(value: any) {
+    let newValue = value;
+    if (value >= 1000) {
+        const suffixes = ["", "k", "m", "b", "t"];
+        const suffixNum = Math.floor(("" + value).length / 3);
+        let shortValue: any;
+        for (let precision = 2; precision >= 1; precision--) {
+            shortValue = parseFloat(
+                (suffixNum != 0
+                        ? value / Math.pow(1000, suffixNum)
+                        : value
+                ).toPrecision(precision)
+            );
+            let dotLessShortValue = (shortValue + "").replace(
+                /[^a-zA-Z 0-9]+/g,
+                ""
+            );
+            if (dotLessShortValue.length <= 2) {
+                break;
+            }
+        }
+        if (shortValue % 1 != 0) shortValue = shortValue.toFixed(1);
+        newValue = shortValue + suffixes[suffixNum];
+    }
+    return newValue;
 }
