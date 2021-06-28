06e8c1d8b5216d28e1dacc55228ed8bbcb282f9e
Andrew <66942330+Noro95@users.noreply.github.com>
2021-06-29 01:57:20 +0200

    Update levelingOperation.ts
    Added user rankcard's updateData fns

--- src/database/functions/levelingOperation.ts
+++ src/database/functions/levelingOperation.ts
@@ -1,45 +1,101 @@
-import {guildXP} from "../../utils/interfaces/levelingInterfaces";
-import {LevelXP} from "../schema";
+import { guildXP } from "../../utils/interfaces/levelingInterfaces";
+import { LevelXP } from "../schema";
 
 export async function getLevel(guild?: string) {
-    const guildXP: guildXP = await LevelXP.findOne({guild});
-    return guildXP;
+  const guildXP: guildXP = await LevelXP.findOne({ guild });
+  return guildXP;
 }
 
 export function updateUserXP(
-    user: string,
-    xp: number,
-    level: number,
-    guild?: string
+  user: string,
+  xp: number,
+  level: number,
+  guild?: string
 ) {
-    LevelXP.updateOne(
-        {guild, "users.user": user},
-        {
-            $set: {
-                "users.$.xp": xp,
-                "users.$.level": level,
-            },
-        }
-    ).catch((err: Error) => console.log(err));
+  LevelXP.updateOne(
+    { guild, "users.user": user },
+    {
+      $set: {
+        "users.$.xp": xp,
+        "users.$.level": level,
+      },
+    }
+  ).catch((err: Error) => console.log(err));
 }
-
+export function updateUserBackground(
+  user: string,
+  guild: string,
+  background: string
+) {
+  LevelXP.updateOne(
+    { guild, "users.user": user },
+    {
+      $set: {
+        "users.$.background": background,
+      },
+    }
+  ).catch((err: Error) => console.log(err));
+}
+export function updateUserOpacity(
+  user: string,
+  guild: string,
+  opacity: number
+) {
+  LevelXP.updateOne(
+    { guild, "users.user": user },
+    {
+      $set: {
+        "users.$.opacity": opacity,
+      },
+    }
+  ).catch((err: Error) => console.log(err));
+}
+export function updateUserTrackColor(
+  user: string,
+  guild: string,
+  trackColor: string
+) {
+  LevelXP.updateOne(
+    { guild, "users.user": user },
+    {
+      $set: {
+        "users.$.trackColor": trackColor,
+      },
+    }
+  ).catch((err: Error) => console.log(err));
+}
+export function updateUserTextColor(
+  user: string,
+  guild: string,
+  textColor: string
+) {
+  LevelXP.updateOne(
+    { guild, "users.user": user },
+    {
+      $set: {
+        "users.$.textColor": textColor,
+      },
+    }
+  ).catch((err: Error) => console.log(err));
+}
 export function initUserXP(user: string, guild?: string) {
-    LevelXP.updateOne(
-        {guild},
-        {
-            $push: {
-                users: {
-                    user: user,
-                    xp: 0,
-                    level: 0,
-                    background: 'https://cdn.discordapp.com/attachments/791301593391562752/856879146175954954/rankcard2.png',
-                    opacity: 0.5,
-                    trackColor: "#21cc87",
-                    textColor: "#554b58"
-                },
-            },
-        }
-    ).catch((err:Error)=>{
-        throw  err;
-    })
+  LevelXP.updateOne(
+    { guild },
+    {
+      $push: {
+        users: {
+          user: user,
+          xp: 0,
+          level: 0,
+          background:
+            "https://cdn.discordapp.com/attachments/791301593391562752/856879146175954954/rankcard2.png",
+          opacity: 0.7,
+          trackColor: "#21cc87",
+          textColor: "#f5deb3",
+        },
+      },
+    }
+  ).catch((err: Error) => {
+    throw err;
+  });
 }
