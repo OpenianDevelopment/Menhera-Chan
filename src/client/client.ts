bf4d382366a893eaae30bee739c09d5d513c36da
Rohan Kumar <rohan.shuvam@gmail.com>
2021-09-08 23:04:24 +0530

    Added commands and event collection. Added guildSetting collection to cache data

---
+++ src/client/client.ts
@@ -0,0 +1,23 @@
+import { Client, ClientOptions, Collection } from "discord.js";
+import BaseCommand from "../structures/BaseCommand";
+import BaseEvent from "../structures/BaseEvent";
+import { GuildSettings } from "../utils/GlobalType";
+
+export default class DiscordClient extends Client {
+    private _commands = new Collection<string, BaseCommand>();
+    private _events = new Collection<string, BaseEvent>();
+    private _guildSettings = new Collection<string, GuildSettings>();
+    constructor(options: ClientOptions) {
+        super(options);
+    }
+
+    get commands(): Collection<string, BaseCommand> {
+        return this._commands;
+    }
+    get events(): Collection<string, BaseEvent> {
+        return this._events;
+    }
+    get guildSettings(): Collection<string, GuildSettings> {
+        return this._guildSettings;
+    }
+}
---
+++ src/utils/GlobalType.ts
@@ -0,0 +1,41 @@
+export interface GuildFeatureSettings {
+    expSystem: boolean;
+    antispamSystem: boolean;
+    inviteSystem: boolean;
+    welcomeSystem: boolean;
+}
+export interface ExpSystemSettings {
+    increment: number;
+    timeDifference: number;
+    blacklistChannel: Array<string>;
+    expLogChannel?: string;
+}
+export interface AntispamSystemSettings {
+    messageCount: number;
+    timeDifference: number;
+    antispamChannel: Array<string>;
+    warnUser: boolean;
+    muteUser: boolean;
+    deleteMessage: boolean;
+}
+
+export interface moderationSystemSettings {
+    modLogChannel?: string;
+    muteRole?: string;
+}
+
+export interface welcomeSystemSettings {
+    welcomeDM: boolean;
+    welcomeChannelMessage: boolean;
+    welcomeChannel: string;
+    welcomeMessage: string;
+    welcomeRoles: Array<string>;
+}
+
+export interface GuildSettings {
+    featureSettings: GuildFeatureSettings;
+    expSettings?: ExpSystemSettings;
+    antispamSettings?: AntispamSystemSettings;
+    moderationSettings?: moderationSystemSettings;
+    welcomeSettings?: welcomeSystemSettings;
+}
