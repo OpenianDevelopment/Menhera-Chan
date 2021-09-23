856cc31a1fca4df8b9bcfa5865256ccd363e88ef
Rohan Kumar <rohan.shuvam@gmail.com>
2021-09-23 22:21:26 +0530

    Fixed RP command

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -20,315 +20,47 @@ const commands: ApplicationCommandData[] = [
                 name: "user",
                 description: "wanted avatar's owner",
                 type: "USER",
-                required: false,
             },
         ],
     },
     {
-        name: "rp",
-        description: "uh.. idk",
+        name: "role-play",
+        description: "Play role play",
         type: "CHAT_INPUT",
         options: [
             {
-                name: "kiss",
-                description:
-                    "just a normal kiss, not kiss of death or whatever",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
+                name: "rp-type",
+                description: "which roleplay to use",
+                type: "STRING",
+                choices: [
+                    { name: "Kiss", value: "kiss" },
+                    { name: "Lick", value: "lick" },
+                    { name: "Yeet", value: "yeet" },
+                    { name: "Cuddle", value: "cuddle" },
+                    { name: "Bite", value: "bite" },
+                    { name: " Pat", value: "pat" },
+                    { name: "Bully", value: "bully" },
+                    { name: "High-Five", value: "highfive" },
+                    { name: "Kill", value: "kill" },
+                    { name: "Cry", value: "cry" },
+                    { name: "Tickle", value: "tickle" },
+                    { name: "Punch", value: "punch" },
+                    { name: "Smile", value: "smile" },
+                    { name: "Greet", value: "greet" },
+                    { name: "Tsundere", value: "tsundere" },
                 ],
+                required: true,
             },
             {
-                name: "lick",
-                description: "just a normal lic... wait tf?",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
+                name: "user",
+                description: "User to do role play with",
+                type: "USER",
+                required: true,
             },
             {
-                name: "yeet",
-                description: "To literally yeet someone away",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "cuddle",
-                description: "cuddling with qts, like menhera u know",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "bite",
-                description: "Hope it doesn't bleed",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "pat",
-                description: "a pat, that's it",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "bully",
-                description: "to booli users in a... nice way?",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "highfive",
-                description: "making friend with highfive is good, right?",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "kill",
-                description: "Got bored of a user? just get rid of em",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "cry",
-                description: "Just let it out..",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "tickle",
-                description: "IF you want someone to open to you, tickle them!",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "punch",
-                description: "Punching bad guys to stay safe",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "smile",
-                description: "Happy? express it out!",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "greet",
-                description: "Saw a qt? greet them! quickly!!",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
-            },
-            {
-                name: "tsundere",
-                description: "I-it's not like i m-made this f-f-for m-myself",
-                type: "SUB_COMMAND",
-                options: [
-                    {
-                        name: "user",
-                        description: "The user to use the rp on",
-                        type: "USER",
-                        required: true,
-                    },
-                    {
-                        name: "text",
-                        description:
-                            "You message to the user, if you want to tho",
-                        type: "STRING",
-                        required: false,
-                    },
-                ],
+                name: "message",
+                description: "Your custom message",
+                type: "STRING",
             },
         ],
     },
