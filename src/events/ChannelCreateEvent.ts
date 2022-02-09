2ef58c1d51c91b56a6ba4defa6a0a4dd3f1c9c02
july12123 <52258148+july12123@users.noreply.github.com>
2022-02-09 08:34:27 -0500

    modlog stuff

---
+++ src/events/ChannelCreateEvent.ts
@@ -0,0 +1,23 @@
+import BaseEvent from "../structures/BaseEvent";
+import DiscordClient from "../client/client";
+import { GuildChannel, TextChannel } from "discord.js";
+
+export default class Event extends BaseEvent {
+    constructor() {
+        super("channelCreate");
+    }
+    async run(client: DiscordClient,channel:GuildChannel) {
+        
+        var guildOption = client.guildSettings.get(channel.guildId)?.moderationSettings
+        // make message here as embed
+
+        //make this a function
+        if(!guildOption?.modLogChannel)return
+        var guild = channel.guild
+        var Modchannel = await guild.channels.fetch(guildOption.modLogChannel) as TextChannel
+        if(!Modchannel)return
+        Modchannel.send("embed here")
+
+        //make this a function
+    }
+}
---
+++ src/events/RoleUpdateEvent.ts
@@ -0,0 +1,12 @@
+import BaseEvent from "../structures/BaseEvent";
+import DiscordClient from "../client/client";
+import { Role } from "discord.js";
+
+export default class Event extends BaseEvent {
+    constructor() {
+        super("roleUpdate");
+    }
+    async run(client: DiscordClient,newRole:Role,oldRole:Role) {
+        client.
+    }
+}
