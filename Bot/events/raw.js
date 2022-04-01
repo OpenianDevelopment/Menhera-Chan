ddbd7143d0c3d0cac65a4283882e0cedaa67ba28
NORO3618 <66942330+Noro95@users.noreply.github.com>
2022-04-01 17:11:48 +0200

    Update raw.js

--- Bot/events/raw.js
+++ Bot/events/raw.js
@@ -1,7 +1,6 @@
 module.exports = (client, raw) => {
 	if (raw.t === "INTERACTION_CREATE") {
 		let command = client.SlashCommands.get(raw.d.data.name);
-		console.log(raw.d)
 		if (!command) return;
 		let guild = client.guilds.cache.get(raw.d.guild_id);
 		try {
